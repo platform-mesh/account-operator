64423d51390a11feb475291a2bdfd58ed44ced4f
Arturs Jefimovs <arturefimov@gmail.com>
2025-08-14 20:35:16 +0300

    feat: subroutine custom workspace creation logic

--- pkg/subroutines/workspacetype.go
+++ pkg/subroutines/workspacetype.go
@@ -2,14 +2,20 @@ package subroutines
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	kcptenancyv1alpha "github.com/kcp-dev/kcp/sdk/apis/tenancy/v1alpha1"
+	corev1alpha1 "github.com/platform-mesh/account-operator/api/v1alpha1"
 	"github.com/platform-mesh/golang-commons/controller/lifecycle/runtimeobject"
 	"github.com/platform-mesh/golang-commons/errors"
 	"github.com/platform-mesh/golang-commons/logger"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 const WorkspaceTypeSubroutineName = "WorkspaceTypeSubroutine"
@@ -33,7 +39,33 @@ func (r *WorkspaceTypeSubroutine) Finalize(ctx context.Context, ro runtimeobject
 }
 
 func (r *WorkspaceTypeSubroutine) Process(ctx context.Context, ro runtimeobject.RuntimeObject) (ctrl.Result, errors.OperatorError) {
-	// TODO: implement creation of custom workspace
-	logger.LoadLoggerFromContext(ctx).Debug().Str("subroutine", WorkspaceTypeSubroutineName).Msg("placeholder executed")
+	acct := ro.(*corev1alpha1.Account)
+	if acct.Spec.Type != corev1alpha1.AccountTypeOrg {
+		return ctrl.Result{}, nil
+	}
+	log := logger.LoadLoggerFromContext(ctx)
+	cn := MustGetClusteredName(ctx, ro)
+
+	base := &kcptenancyv1alpha.WorkspaceType{}
+	if err := r.client.Get(ctx, client.ObjectKey{Name: "org"}, base); err != nil {
+		if kerrors.IsNotFound(err) {
+			delay := r.limiter.When(cn)
+			log.Info().Str("account", acct.Name).Msg("base org WorkspaceType not found yet; requeue")
+			return ctrl.Result{RequeueAfter: delay}, nil
+		}
+		return ctrl.Result{}, errors.NewOperatorError(err, true, true)
+	}
+
+	customName := fmt.Sprintf("%s-org", acct.Name)
+	custom := &kcptenancyv1alpha.WorkspaceType{ObjectMeta: metav1.ObjectMeta{Name: customName}}
+	_, err := controllerutil.CreateOrUpdate(ctx, r.client, custom, func() error {
+		custom.Spec = base.Spec
+		return controllerutil.SetOwnerReference(acct, custom, r.client.Scheme())
+	})
+	if err != nil {
+		return ctrl.Result{}, errors.NewOperatorError(err, true, true)
+	}
+	r.limiter.Forget(cn)
+	log.Debug().Str("customWorkspaceType", customName).Msg("custom org workspace type ensured")
 	return ctrl.Result{}, nil
 }
