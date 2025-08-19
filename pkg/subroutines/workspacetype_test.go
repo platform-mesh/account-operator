b924053ca8d2c0c664068d394fc608b8abe7caab
Arturs Jefimovs <arturefimov@gmail.com>
2025-08-19 20:30:07 +0300

    feat: new tests for workspacetype to increase coverage

--- pkg/subroutines/workspacetype_test.go
+++ pkg/subroutines/workspacetype_test.go
@@ -6,12 +6,20 @@ import (
 	"time"
 
 	kcptenancyv1alpha "github.com/kcp-dev/kcp/sdk/apis/tenancy/v1alpha1"
+	"github.com/kcp-dev/logicalcluster/v3"
+	corev1alpha1 "github.com/platform-mesh/account-operator/api/v1alpha1"
+	operatorconfig "github.com/platform-mesh/account-operator/internal/config"
+	"github.com/platform-mesh/account-operator/pkg/subroutines"
+	platformmeshcontext "github.com/platform-mesh/golang-commons/context"
+	"github.com/platform-mesh/golang-commons/logger"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/kontext"
 )
 
 func TestWorkspaceTypeSpecCopy(t *testing.T) {
@@ -46,4 +54,49 @@ func TestWorkspaceTypeSpecCopy(t *testing.T) {
 		}
 		return fetchedOrg.Spec.DefaultChildWorkspaceType != nil && string(fetchedOrg.Spec.DefaultChildWorkspaceType.Name) == customAccName
 	}, 2*time.Second, 100*time.Millisecond)
+}
+
+func TestWorkspaceTypeSubroutine_Process_FallbackClient(t *testing.T) {
+	// Arrange
+	scheme := runtime.NewScheme()
+	utilruntime.Must(corev1alpha1.AddToScheme(scheme))
+	utilruntime.Must(kcptenancyv1alpha.AddToScheme(scheme))
+
+	acct := &corev1alpha1.Account{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-org", UID: "1234"},
+		Spec:       corev1alpha1.AccountSpec{Type: corev1alpha1.AccountTypeOrg},
+	}
+
+	// Create a fake client that will act as both the regular and root client
+	baseOrgWT := &kcptenancyv1alpha.WorkspaceType{ObjectMeta: metav1.ObjectMeta{Name: "org"}}
+	baseAccWT := &kcptenancyv1alpha.WorkspaceType{ObjectMeta: metav1.ObjectMeta{Name: "account"}}
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(baseOrgWT, baseAccWT).Build()
+
+	// Use the constructor WITHOUT the root client to test the fallback logic
+	sub := subroutines.NewWorkspaceTypeSubroutine(fakeClient)
+
+	cfg := operatorconfig.OperatorConfig{}
+	cfg.Kcp.ProviderWorkspace = "root"
+
+	log, err := logger.New(logger.DefaultConfig())
+	require.NoError(t, err)
+	ctx, _, _ := platformmeshcontext.StartContext(log, cfg, 1*time.Minute)
+
+	// Set a logical cluster on the context, as the real controller would
+	ctx = kontext.WithCluster(ctx, logicalcluster.Name("orgs:root-org"))
+
+	// Act
+	_, opErr := sub.Process(ctx, acct)
+
+	// Assert
+	require.Nil(t, opErr)
+
+	// Check that the custom WorkspaceTypes were created correctly
+	customOrgWT := &kcptenancyv1alpha.WorkspaceType{}
+	err = fakeClient.Get(ctx, types.NamespacedName{Name: "test-org-org"}, customOrgWT)
+	require.NoError(t, err, "custom org workspacetype should be created")
+
+	customAccWT := &kcptenancyv1alpha.WorkspaceType{}
+	err = fakeClient.Get(ctx, types.NamespacedName{Name: "test-org-acc"}, customAccWT)
+	require.NoError(t, err, "custom account workspacetype should be created")
 }
