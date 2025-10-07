a32b43697b7e0a2ba35955ca363400e85cfe7891
Arturs Jefimovs <arturefimov@gmail.com>
2025-10-07 15:23:56 +0300

    fix: openfga alias in mock

--- pkg/subroutines/doc.go
+++ pkg/subroutines/doc.go
@@ -1,3 +1,3 @@
 package subroutines
 
-//go:generate bash -lc "cd ../.. && go run -mod=mod github.com/vektra/mockery/v3"
+//go:generate bash -lc "cd ../.. && go run -mod=mod github.com/vektra/mockery/v3@v3.5.5"
