871bc4bc9a1a3704d02ef3573607080d5155fb65
Simon Tesar <simon@simontesar.com>
2026-01-22 05:43:36 +0000

    refactor: test: rename struct parameter
    On-behalf-of: @SAP <simon@simontesar.com>

--- internal/controller/account_controller_test.go
+++ internal/controller/account_controller_test.go
@@ -114,15 +114,15 @@ func TestAccountTestSuite(t *testing.T) {
 	suite.Run(t, new(AccountTestSuite))
 }
 
-func (suite *AccountTestSuite) SetupSuite() {
+func (s *AccountTestSuite) SetupSuite() {
 	logConfig := logger.DefaultConfig()
 	logConfig.NoJSON = true
 	logConfig.Name = "AccountTestSuite"
 	logConfig.Level = "debug"
 
 	log, err := logger.New(logConfig)
-	suite.Require().NoError(err)
-	suite.log = log
+	s.Require().NoError(err)
+	s.log = log
 	ctrl.SetLogger(log.Logr())
 
 	cfg := config.OperatorConfig{}
@@ -132,146 +132,146 @@ func (suite *AccountTestSuite) SetupSuite() {
 	cfg.Subroutines.WorkspaceType.Enabled = true
 	cfg.Kcp.ProviderWorkspace = "root"
 
-	suite.ctx, suite.cancel, _ = platformmeshcontext.StartContext(log, cfg, 2*time.Minute)
+	s.ctx, s.cancel, _ = platformmeshcontext.StartContext(log, cfg, 2*time.Minute)
 	testEnvLogger := log.ComponentLogger("kcpenvtest")
 
-	suite.scheme = runtime.NewScheme()
-	utilruntime.Must(v1alpha1.AddToScheme(suite.scheme))
-	utilruntime.Must(v1.AddToScheme(suite.scheme))
-	utilruntime.Must(kcpapisv1alpha1.AddToScheme(suite.scheme))
-	utilruntime.Must(kcpcorev1alpha.AddToScheme(suite.scheme))
-	utilruntime.Must(kcptenancyv1alpha.AddToScheme(suite.scheme))
+	s.scheme = runtime.NewScheme()
+	utilruntime.Must(v1alpha1.AddToScheme(s.scheme))
+	utilruntime.Must(v1.AddToScheme(s.scheme))
+	utilruntime.Must(kcpapisv1alpha1.AddToScheme(s.scheme))
+	utilruntime.Must(kcpcorev1alpha.AddToScheme(s.scheme))
+	utilruntime.Must(kcptenancyv1alpha.AddToScheme(s.scheme))
 
-	suite.testEnv = &envtest.Environment{AttachKcpOutput: false}
-	suite.testEnv.BinaryAssetsDirectory = "../../bin"
-	suite.testEnv.KcpStartTimeout = 2 * time.Minute
-	suite.testEnv.KcpStopTimeout = 30 * time.Second
+	s.testEnv = &envtest.Environment{AttachKcpOutput: false}
+	s.testEnv.BinaryAssetsDirectory = "../../bin"
+	s.testEnv.KcpStartTimeout = 2 * time.Minute
+	s.testEnv.KcpStopTimeout = 30 * time.Second
 
-	kcpConfig, err := suite.testEnv.Start()
-	suite.Require().NoError(err)
-	suite.mcc, err = mcc.New(kcpConfig, client.Options{
-		Scheme: suite.scheme,
+	kcpConfig, err := s.testEnv.Start()
+	s.Require().NoError(err)
+	s.mcc, err = mcc.New(kcpConfig, client.Options{
+		Scheme: s.scheme,
 	})
-	suite.Require().NoError(err)
+	s.Require().NoError(err)
 
 	// Setup root workspace
 	providerCfg := rest.CopyConfig(kcpConfig)
 	providerCfg.Host += "/clusters/root:platform-mesh-system"
-	suite.Require().NoError(err)
-	c := suite.mcc.Cluster(logicalcluster.NewPath("root"))
-	suite.Require().NoError(err)
+	s.Require().NoError(err)
+	c := s.mcc.Cluster(logicalcluster.NewPath("root"))
+	s.Require().NoError(err)
 	WaitForWorkspaceWithTimeout(c, "root", testEnvLogger, 15*time.Second)
 
 	// Setup root workspace and wait for workspaces to appear
-	rootClient := suite.mcc.Cluster(logicalcluster.NewPath("root"))
+	rootClient := s.mcc.Cluster(logicalcluster.NewPath("root"))
 	for _, data := range rootWorkspaceTypes {
 		var wt kcptenancyv1alpha1.WorkspaceType
 		err := yaml.Unmarshal(data, &wt)
-		suite.Require().NoError(err, "Unmarshalling embedded data")
+		s.Require().NoError(err, "Unmarshalling embedded data")
 
-		err = rootClient.Create(suite.ctx, &wt)
-		suite.Require().NoError(err, "Creating unmarshalled object")
+		err = rootClient.Create(s.ctx, &wt)
+		s.Require().NoError(err, "Creating unmarshalled object")
 	}
 	for _, data := range rootWorkspaces {
 		var ws kcptenancyv1alpha1.Workspace
 		err := yaml.Unmarshal(data, &ws)
-		suite.Require().NoError(err, "Unmarshalling embedded data")
+		s.Require().NoError(err, "Unmarshalling embedded data")
 
-		err = rootClient.Create(suite.ctx, &ws)
-		suite.Require().NoError(err, "Creating unmarshalled object")
+		err = rootClient.Create(s.ctx, &ws)
+		s.Require().NoError(err, "Creating unmarshalled object")
 	}
 
 	// Setup platform-mesh-system workspace
 	WaitForWorkspaceWithTimeout(c, "platform-mesh-system", testEnvLogger, 15*time.Second)
-	pmsClient := suite.mcc.Cluster(logicalcluster.NewPath("root:platform-mesh-system"))
+	pmsClient := s.mcc.Cluster(logicalcluster.NewPath("root:platform-mesh-system"))
 	for _, data := range platformMeshSystemAPIResourceSchemas {
 		var ars kcpapisv1alpha1.APIResourceSchema
 		err := yaml.Unmarshal(data, &ars)
-		suite.Require().NoError(err, "Unmarshalling embedded data")
+		s.Require().NoError(err, "Unmarshalling embedded data")
 
 		testEnvLogger.Debug().Msgf("Creating APIResourceSchema %s", ars.Name)
-		err = pmsClient.Create(suite.ctx, &ars)
-		suite.Require().NoError(err, "Creating unmarshalled object")
+		err = pmsClient.Create(s.ctx, &ars)
+		s.Require().NoError(err, "Creating unmarshalled object")
 	}
 
 	var aePlatformMesh, aeTenancy kcpapisv1alpha1.APIExport
-	err = rootClient.Get(suite.ctx, types.NamespacedName{Name: "tenancy.kcp.io"}, &aeTenancy)
-	suite.Require().NoError(err)
+	err = rootClient.Get(s.ctx, types.NamespacedName{Name: "tenancy.kcp.io"}, &aeTenancy)
+	s.Require().NoError(err)
 	err = yaml.Unmarshal(apiexportCorePlatformMeshIoYAML, &aePlatformMesh)
-	suite.Require().NoError(err, "Unmarshalling embedded data")
+	s.Require().NoError(err, "Unmarshalling embedded data")
 	aePlatformMesh.Spec.PermissionClaims[1].IdentityHash = aeTenancy.Status.IdentityHash
 	aePlatformMesh.Spec.PermissionClaims[2].IdentityHash = aeTenancy.Status.IdentityHash
-	err = pmsClient.Create(suite.ctx, &aePlatformMesh)
-	suite.Require().NoError(err, "Creating unmarshalled object")
+	err = pmsClient.Create(s.ctx, &aePlatformMesh)
+	s.Require().NoError(err, "Creating unmarshalled object")
 
 	time.Sleep(time.Second * 10) // race, auf was muss man hier warten?
 	var axes kcpapisv1alpha1.APIExportEndpointSlice
 	err = yaml.Unmarshal(apiexportendpointsliceCorePlatformMeshOrgYAML, &axes)
-	suite.Require().NoError(err, "Unmarshalling embedded data")
-	err = pmsClient.Create(suite.ctx, &axes)
-	suite.Require().NoError(err, "Creating unmarshalled object")
+	s.Require().NoError(err, "Unmarshalling embedded data")
+	err = pmsClient.Create(s.ctx, &axes)
+	s.Require().NoError(err, "Creating unmarshalled object")
 
 	// Setup orgs workspace with test Account
 	WaitForWorkspaceWithTimeout(c, "orgs", testEnvLogger, 15*time.Second)
-	orgsClient := suite.mcc.Cluster(logicalcluster.NewPath("root:orgs"))
+	orgsClient := s.mcc.Cluster(logicalcluster.NewPath("root:orgs"))
 	var acc v1alpha1.Account
 	err = yaml.Unmarshal(accountRootOrgYAML, &acc)
-	suite.Require().NoError(err, "Unmarshalling embedded data")
-	err = orgsClient.Create(suite.ctx, &acc)
-	suite.Require().NoError(err, "Creating unmarshalled object")
+	s.Require().NoError(err, "Unmarshalling embedded data")
+	err = orgsClient.Create(s.ctx, &acc)
+	s.Require().NoError(err, "Creating unmarshalled object")
 
 	axplogr := log.ComponentLogger("apiexport_provider").Logr()
-	suite.provider, err = apiexport.New(providerCfg, "core.platform-mesh.io", apiexport.Options{
-		Scheme: suite.scheme,
+	s.provider, err = apiexport.New(providerCfg, "core.platform-mesh.io", apiexport.Options{
+		Scheme: s.scheme,
 		Log:    &axplogr,
 	})
-	suite.Require().NoError(err)
+	s.Require().NoError(err)
 
 	mcOpts := mcmanager.Options{
-		Scheme: suite.scheme,
+		Scheme: s.scheme,
 		Metrics: metricsserver.Options{
 			BindAddress: "0",
 		},
-		BaseContext: func() context.Context { return suite.ctx },
+		BaseContext: func() context.Context { return s.ctx },
 	}
 
-	suite.multiClusterManager, err = mcmanager.New(providerCfg, suite.provider, mcOpts)
-	suite.Require().NoError(err)
+	s.multiClusterManager, err = mcmanager.New(providerCfg, s.provider, mcOpts)
+	s.Require().NoError(err)
 
-	mockClient := mocks.NewOpenFGAServiceClient(suite.T())
-	accountReconciler := controller.NewAccountReconciler(log, suite.multiClusterManager, cfg, orgsClient, mockClient)
+	mockClient := mocks.NewOpenFGAServiceClient(s.T())
+	accountReconciler := controller.NewAccountReconciler(log, s.multiClusterManager, cfg, orgsClient, mockClient)
 
-	managerCtx, cancel := context.WithCancel(suite.ctx)
+	managerCtx, cancel := context.WithCancel(s.ctx)
 	eg, egCtx := errgroup.WithContext(managerCtx)
 	eg.Go(func() error {
-		return suite.multiClusterManager.Start(egCtx)
+		return s.multiClusterManager.Start(egCtx)
 	})
 
-	suite.T().Cleanup(func() {
+	s.T().Cleanup(func() {
 		cancel()
 		if err := eg.Wait(); err != nil && !errors.Is(err, context.Canceled) {
-			suite.T().Logf("controller manager exited with error: %v", err)
+			s.T().Logf("controller manager exited with error: %v", err)
 		}
 	})
 
 	dCfg := &platformmeshconfig.CommonServiceConfig{}
-	suite.Require().NoError(accountReconciler.SetupWithManager(suite.multiClusterManager, dCfg, log))
+	s.Require().NoError(accountReconciler.SetupWithManager(s.multiClusterManager, dCfg, log))
 
-	suite.rootOrgsClient = suite.mcc.Cluster(logicalcluster.NewPath("root:orgs"))
-	suite.rootOrgsDefaultClient = suite.mcc.Cluster(logicalcluster.NewPath("root:orgs:default"))
-	suite.Require().NoError(WaitForWorkspaceWithTimeout(orgsClient, "default", testEnvLogger, time.Minute))
+	s.rootOrgsClient = s.mcc.Cluster(logicalcluster.NewPath("root:orgs"))
+	s.rootOrgsDefaultClient = s.mcc.Cluster(logicalcluster.NewPath("root:orgs:default"))
+	s.Require().NoError(WaitForWorkspaceWithTimeout(orgsClient, "default", testEnvLogger, time.Minute))
 }
 
-func (suite *AccountTestSuite) TearDownSuite() {
-	if suite.cancel != nil {
-		suite.cancel(fmt.Errorf("tearing down test suite"))
+func (s *AccountTestSuite) TearDownSuite() {
+	if s.cancel != nil {
+		s.cancel(fmt.Errorf("tearing down test suite"))
 	}
-	if suite.testEnv != nil {
-		_ = suite.testEnv.Stop()
+	if s.testEnv != nil {
+		_ = s.testEnv.Stop()
 	}
 }
 
-func (suite *AccountTestSuite) TestAddingFinalizer() {
+func (s *AccountTestSuite) TestAddingFinalizer() {
 	testContext := context.Background()
 	accountName := "test-account-finalizer"
 
@@ -284,103 +284,103 @@ func (suite *AccountTestSuite) TestAddingFinalizer() {
 		},
 	}
 
-	suite.Require().NoError(suite.rootOrgsDefaultClient.Create(testContext, account))
+	s.Require().NoError(s.rootOrgsDefaultClient.Create(testContext, account))
 
 	createdAccount := v1alpha1.Account{}
-	suite.Assert().Eventually(func() bool {
-		err := suite.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName, Namespace: defaultNamespace}, &createdAccount)
+	s.Assert().Eventually(func() bool {
+		err := s.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName, Namespace: defaultNamespace}, &createdAccount)
 
 		return err == nil && len(createdAccount.Finalizers) == 3
 	}, defaultTestTimeout*2, defaultTickInterval)
 
-	suite.ElementsMatch([]string{"workspacetype.core.platform-mesh.io/finalizer", "account.core.platform-mesh.io/finalizer", "account.core.platform-mesh.io/info"}, createdAccount.Finalizers)
+	s.ElementsMatch([]string{"workspacetype.core.platform-mesh.io/finalizer", "account.core.platform-mesh.io/finalizer", "account.core.platform-mesh.io/info"}, createdAccount.Finalizers)
 }
 
-func (suite *AccountTestSuite) TestWorkspaceCreation() {
+func (s *AccountTestSuite) TestWorkspaceCreation() {
 	testContext := context.Background()
 	accountName := "test-account-ws-creation"
 	account := &v1alpha1.Account{ObjectMeta: metav1.ObjectMeta{Name: accountName}, Spec: v1alpha1.AccountSpec{Type: v1alpha1.AccountTypeAccount}}
 
-	suite.Require().NoError(suite.rootOrgsDefaultClient.Create(testContext, account))
+	s.Require().NoError(s.rootOrgsDefaultClient.Create(testContext, account))
 
 	createdWorkspace := kcptenancyv1alpha.Workspace{}
-	suite.Assert().Eventually(func() bool {
-		if err := suite.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName}, &createdWorkspace); err != nil {
+	s.Assert().Eventually(func() bool {
+		if err := s.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName}, &createdWorkspace); err != nil {
 			return false
 		}
 		return createdWorkspace.Status.Phase == kcpcorev1alpha.LogicalClusterPhaseReady
 	}, defaultTestTimeout, defaultTickInterval)
 
 	updatedAccount := &v1alpha1.Account{}
-	suite.Assert().Eventually(func() bool {
-		if err := suite.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName}, updatedAccount); err != nil {
+	s.Assert().Eventually(func() bool {
+		if err := s.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName}, updatedAccount); err != nil {
 			return false
 		}
 		return meta.IsStatusConditionTrue(updatedAccount.Status.Conditions, "WorkspaceSubroutine_Ready")
 	}, defaultTestTimeout, defaultTickInterval)
 
-	suite.verifyWorkspace(testContext, "default", accountName)
-	suite.verifyCondition(updatedAccount.Status.Conditions, "WorkspaceSubroutine_Ready", metav1.ConditionTrue, "Complete")
+	s.verifyWorkspace(testContext, "default", accountName)
+	s.verifyCondition(updatedAccount.Status.Conditions, "WorkspaceSubroutine_Ready", metav1.ConditionTrue, "Complete")
 }
 
-func (suite *AccountTestSuite) TestAccountInfoCreationForOrganization() {
+func (s *AccountTestSuite) TestAccountInfoCreationForOrganization() {
 	testContext := context.Background()
 	accountName := "test-org-account"
 	account := &v1alpha1.Account{ObjectMeta: metav1.ObjectMeta{Name: accountName}, Spec: v1alpha1.AccountSpec{Type: v1alpha1.AccountTypeOrg}}
 
-	suite.Require().NoError(suite.rootOrgsClient.Create(testContext, account))
+	s.Require().NoError(s.rootOrgsClient.Create(testContext, account))
 
 	createdAccount := &v1alpha1.Account{}
-	suite.Assert().Eventually(func() bool {
-		if err := suite.rootOrgsClient.Get(testContext, types.NamespacedName{Name: accountName}, createdAccount); err != nil {
+	s.Assert().Eventually(func() bool {
+		if err := s.rootOrgsClient.Get(testContext, types.NamespacedName{Name: accountName}, createdAccount); err != nil {
 			return false
 		}
 		return meta.IsStatusConditionTrue(createdAccount.Status.Conditions, "AccountInfoSubroutine_Ready")
 	}, defaultTestTimeout, defaultTickInterval)
 
 	accountInfo := &v1alpha1.AccountInfo{}
-	suite.Assert().Eventually(func() bool {
-		if err := suite.rootOrgsDefaultClient.Get(testContext, client.ObjectKey{Name: accountinfo.DefaultAccountInfoName}, accountInfo); err != nil {
+	s.Assert().Eventually(func() bool {
+		if err := s.rootOrgsDefaultClient.Get(testContext, client.ObjectKey{Name: accountinfo.DefaultAccountInfoName}, accountInfo); err != nil {
 			return false
 		}
 		return accountInfo.Spec.Account.Type == v1alpha1.AccountTypeOrg
 	}, defaultTestTimeout, defaultTickInterval)
 }
 
-func (suite *AccountTestSuite) TestWorkspaceFinalizerRemovesWorkspace() {
+func (s *AccountTestSuite) TestWorkspaceFinalizerRemovesWorkspace() {
 	testContext := context.Background()
 	accountName := "test-workspace-finalizer"
 	account := &v1alpha1.Account{ObjectMeta: metav1.ObjectMeta{Name: accountName}, Spec: v1alpha1.AccountSpec{Type: v1alpha1.AccountTypeAccount}}
 
-	suite.Require().NoError(suite.rootOrgsDefaultClient.Create(testContext, account))
+	s.Require().NoError(s.rootOrgsDefaultClient.Create(testContext, account))
 
 	createdWorkspace := kcptenancyv1alpha.Workspace{}
-	suite.Assert().Eventually(func() bool {
-		return suite.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName}, &createdWorkspace) == nil
+	s.Assert().Eventually(func() bool {
+		return s.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName}, &createdWorkspace) == nil
 	}, defaultTestTimeout, defaultTickInterval)
 
-	suite.Require().NoError(suite.rootOrgsDefaultClient.Delete(testContext, account))
+	s.Require().NoError(s.rootOrgsDefaultClient.Delete(testContext, account))
 
-	suite.Assert().Eventually(func() bool {
-		err := suite.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName}, &kcptenancyv1alpha.Workspace{})
+	s.Assert().Eventually(func() bool {
+		err := s.rootOrgsDefaultClient.Get(testContext, types.NamespacedName{Name: accountName}, &kcptenancyv1alpha.Workspace{})
 		return kerrors.IsNotFound(err)
 	}, defaultTestTimeout, defaultTickInterval)
 }
 
-func (suite *AccountTestSuite) verifyWorkspace(ctx context.Context, orgName, accountName string) {
+func (s *AccountTestSuite) verifyWorkspace(ctx context.Context, orgName, accountName string) {
 	workspace := &kcptenancyv1alpha.Workspace{}
-	suite.Require().NoError(suite.rootOrgsDefaultClient.Get(ctx, types.NamespacedName{Name: accountName}, workspace))
-	suite.Equal(accountName, workspace.Name)
-	suite.NotNil(workspace.Spec.Type)
+	s.Require().NoError(s.rootOrgsDefaultClient.Get(ctx, types.NamespacedName{Name: accountName}, workspace))
+	s.Equal(accountName, workspace.Name)
+	s.NotNil(workspace.Spec.Type)
 	expectedType := kcptenancyv1alpha.WorkspaceTypeName(fmt.Sprintf("%s-acc", orgName))
-	suite.Equal(expectedType, workspace.Spec.Type.Name)
+	s.Equal(expectedType, workspace.Spec.Type.Name)
 }
 
-func (suite *AccountTestSuite) verifyCondition(conditions []metav1.Condition, conditionType string, status metav1.ConditionStatus, reason string) {
-	suite.Assert().True(meta.IsStatusConditionPresentAndEqual(conditions, conditionType, status))
+func (s *AccountTestSuite) verifyCondition(conditions []metav1.Condition, conditionType string, status metav1.ConditionStatus, reason string) {
+	s.Assert().True(meta.IsStatusConditionPresentAndEqual(conditions, conditionType, status))
 	condition := meta.FindStatusCondition(conditions, conditionType)
-	suite.Require().NotNil(condition)
-	suite.Equal(reason, condition.Reason)
+	s.Require().NotNil(condition)
+	s.Equal(reason, condition.Reason)
 }
 
 func WaitForWorkspaceWithTimeout(client client.Client, name string, log *logger.Logger, timeout time.Duration) error {
